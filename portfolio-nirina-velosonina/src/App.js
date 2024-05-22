092c468fc07c7c384f14cad6ec038bf4a3e9f60e
Nirina VELOSONINA <vnirina081@gmail.com>
2024-05-23 00:46:01 +0200

    ajout import Footer dans App et ajout des styles/variables

--- portfolio-nirina-velosonina/src/App.js
+++ portfolio-nirina-velosonina/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
