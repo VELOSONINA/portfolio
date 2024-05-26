67b9fdffedc1b3b9976586d037414c15e9412e91
Nirina VELOSONINA <vnirina081@gmail.com>
2024-05-26 06:13:01 +0200

    ajout de BrowserRouter

--- portfolio-nirina-velosonina/src/App.js
+++ portfolio-nirina-velosonina/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; 
 import Header from './Components/Header';
+import RoutesTree from './Components/RoutesTree';
 import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="app-root">
+        <Header/>
+        <RoutesTree/>
+        <Footer/>
+      </div>
+    </BrowserRouter> 
   );
 }
 
