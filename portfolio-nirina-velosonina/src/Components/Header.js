25209a0e7d6ec95526d052c58d0f02d9fd042457
Nirina VELOSONINA <vnirina081@gmail.com>
2024-05-26 05:56:26 +0200

    ajout de Nav.Link Skills dans Header.js

--- portfolio-nirina-velosonina/src/Components/Header.js
+++ portfolio-nirina-velosonina/src/Components/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function Header() {
+
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#A propos">Nirina VELOSONINA</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav"  className="justify-content-end"> 
-        <Nav className="ml-auto">
-          <Nav.Link href="#A propos">À propos</Nav.Link>
-          <Nav.Link href="#Projets">Projets</Nav.Link>
-          <Nav.Link href="#Contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand href="#A propos">Nirina VELOSONINA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto">
+            <Nav.Link href="#A propos">À propos</Nav.Link>
+            <Nav.Link href="#Projets">Projets</Nav.Link>
+            <Nav.Link href="#Skills">Skills</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
