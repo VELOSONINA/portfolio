40c4b5df5314fd75653812810ce36ed38005d987
Nirina VELOSONINA <vnirina081@gmail.com>
2024-05-26 06:04:20 +0200

    ajout de HomePage dans le composant Pages

---
+++ portfolio-nirina-velosonina/src/Components/Pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeContainer from '../Containers/HomeContainers/HomeContainer.js';
+
+const HomePage = () => {
+  return (
+    <HomeContainer/>
+  );
+};
+
+export default HomePage;
